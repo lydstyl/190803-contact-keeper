c8a42dfaa8aa5d3cd4d8b505330e7ef390e72503
Gabriel Brun <lydstyl@gmail.com>
2019-08-06 13:04:29 +0200

    56 Set & Clear Current Contact

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -2,15 +2,17 @@ import React, { useContext } from 'react';
 import ContactContext from '../../context/contact/contactContext';
 
 import PropTypes from 'prop-types';
+import { set } from 'mongoose';
 
 const ContactItem = ({ contact }) => {
   const contactContext = useContext(ContactContext);
-  const { deleteContact } = contactContext;
+  const { deleteContact, setCurrent, clearCurrent } = contactContext;
 
   const { id, name, email, phone, type } = contact;
 
   const onDelete = () => {
     deleteContact(id);
+    clearCurrent();
   };
 
   return (
@@ -40,7 +42,12 @@ const ContactItem = ({ contact }) => {
         )}
       </ul>
       <p>
-        <button className='btn btn-dark btn-sm'>Edit</button>
+        <button
+          className='btn btn-dark btn-sm'
+          onClick={() => setCurrent(contact)}
+        >
+          Edit
+        </button>
         <button className='btn btn-danger btn-sm' onClick={onDelete}>
           Delete
         </button>
