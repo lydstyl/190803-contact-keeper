a6322cb0f91084c5b7cff4179b73d4fa5d897e11
Gabriel Brun <lydstyl@gmail.com>
2019-08-06 10:07:53 +0200

    51 Cleanup, Navbar & Router

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About This App</h1>
+      <p className='my-1'>
+        This is a full stack React app for keeping contacts
+      </p>
+      <p className='bg-dark'>
+        <strong>Version: </strong>
+      </p>{' '}
+      1.0.0
+    </div>
+  );
+};
+
+export default About;
